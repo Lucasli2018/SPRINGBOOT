f6679113a336334f0af946be2bb89495032f35bc
lucasli <1906859953@qq.com>
2019-09-29 21:27:26 +0800

    springboot服务端数据校验

---
+++ springboot_validate/src/main/java/com/jason/App.java
@@ -0,0 +1,16 @@
+package com.jason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ *Thymeleaf入门案例
+ *
+ */
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
