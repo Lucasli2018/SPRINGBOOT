e4adea927293a2d5fad47cf2ec3e907c7fd5df93
lucasli <1906859953@qq.com>
2019-09-30 00:13:58 +0800

    springboot全局异常处理：@Configuation+@Bean，返回值SimpleMappingExceptionResolver 缺点是：没有异常参数信息

--- springboot_exception/src/main/java/com/jason/exception/GlobalException.java
+++ springboot_exception/src/main/java/com/jason/exception/GlobalException.java
@@ -16,7 +16,7 @@ public class GlobalException {
 	 * 该方法需要返回一个ModelAndView：目的是可以让我们封装异常信息以及视图的指定
 	 * 参数Exception e:会将产生异常对象注入到方法中
 	 */
-	@ExceptionHandler(value={java.lang.ArithmeticException.class})
+	//@ExceptionHandler(value={java.lang.ArithmeticException.class})
 	public ModelAndView arithmeticExceptionHandler(Exception e){
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("error", e.toString());
@@ -29,7 +29,7 @@ public class GlobalException {
 	 * 该方法需要返回一个ModelAndView：目的是可以让我们封装异常信息以及视图的指定
 	 * 参数Exception e:会将产生异常对象注入到方法中
 	 */
-	@ExceptionHandler(value={java.lang.NullPointerException.class})
+	//@ExceptionHandler(value={java.lang.NullPointerException.class})
 	public ModelAndView nullPointerExceptionHandler(Exception e){
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("error", e.toString());
