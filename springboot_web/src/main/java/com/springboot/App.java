fa2b30c7288c54c04917d59b9b04345f215e7857
lucasli <1906859953@qq.com>
2019-09-29 10:21:58 +0800

    springboot整合web

--- springboot_web/src/main/java/com/springboot/App.java
+++ springboot_web/src/main/java/com/springboot/App.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * 启动类
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 @ServletComponentScan
 public class App {
--- springboot_web/src/main/java/com/springboot/App2.java
+++ springboot_web/src/main/java/com/springboot/App2.java
@@ -1,9 +1,13 @@
 package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
+import com.springboot.filter.SecondFilter;
+import com.springboot.listener.SecondListener;
 import com.springboot.servlet.SecondServlet;
 
 
@@ -18,11 +22,32 @@ public class App2 {
 	public static void main(String[] args) {
 		SpringApplication.run(App2.class, args);
 	}
-	
+	/**
+	 * 注册Servlet
+	 * @return
+	 */
 	@Bean
 	public ServletRegistrationBean getServletRegistrationBean(){
 		ServletRegistrationBean bean=new ServletRegistrationBean(new SecondServlet());
 		bean.addUrlMappings("/second");
+		return bean;
+	}
+	/**
+	 * 注册Filter
+	 */
+	@Bean
+	public FilterRegistrationBean getFilterRegistrationBean(){
+		FilterRegistrationBean bean = new FilterRegistrationBean(new SecondFilter());
+		//bean.addUrlPatterns(new String[]{"*.do","*.jsp"});
+		bean.addUrlPatterns("/second");
+		return bean;
+	}
+	/**
+	 * 注册listener
+	 */
+	@Bean
+	public ServletListenerRegistrationBean<SecondListener> getServletListenerRegistrationBean(){
+		ServletListenerRegistrationBean<SecondListener> bean=new ServletListenerRegistrationBean<SecondListener>(new SecondListener());
 		return bean;
 	}
 
