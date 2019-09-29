c131873022d0a515cf62b72b5563881f9a7a43ad
lucasli <1906859953@qq.com>
2019-09-29 12:26:27 +0800

    springboot文件上传

---
+++ springboot_web/src/main/java/com/springboot/controller/FileUploadController.java
@@ -0,0 +1,30 @@
+package com.springboot.controller;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * SpringBoot文件上传
+ *
+ *
+ */
+//@Controller
+@RestController //表示该类下的方法的返回值会自动做json格式的转换
+public class FileUploadController {
+	/*
+	 * 处理文件上传
+	 */
+	@RequestMapping("/fileUploadController")
+	public Map<String, Object> fileUpload(MultipartFile filename)throws Exception{
+		System.out.println(filename.getOriginalFilename());
+		filename.transferTo(new File("e:/"+filename.getOriginalFilename()));
+		Map<String, Object> map = new HashMap<>();
+		map.put("msg", "ok");
+		return map;
+	}
+}
