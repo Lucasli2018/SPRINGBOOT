e00fb780ba33c248a5e96bfd979cdf58839b97f6
lucasli <1906859953@qq.com>
2019-09-30 10:37:00 +0800

    springboot热部署：devtools

---
+++ springboot_hotloader/springboot_devtools/src/main/java/com/jason/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.jason.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * SpringBoot----SpringLoader
+ *
+ *
+ */
+@Controller
+public class UserController {
+	
+	@RequestMapping("/show")
+	public String showPage(){
+		System.out.println("hello...jason！！！");
+		return "index";
+	}
+}
