66ec2301aa10c0f2ea40e615420886d80c6a6144
lucasli <1906859953@qq.com>
2019-10-01 10:05:36 +0800

    springboot整合缓存框架ehcache：@Cacheable、@CacheEvict

--- springboot_cache/springboot_ehcache/src/main/java/com/jason/service/impl/UserServiceImpl.java
+++ springboot_cache/springboot_ehcache/src/main/java/com/jason/service/impl/UserServiceImpl.java
@@ -3,10 +3,12 @@ package com.jason.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jason.dao.UserRepository;
 import com.jason.pojo.User;
@@ -17,12 +19,14 @@ import com.jason.service.UserService;
  *
  */
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Override
+	@Cacheable(value="users")
 	public List<User> findUserAll() {
 		return this.userRepository.findAll();
 	}
@@ -35,11 +39,14 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
+	@Cacheable(value="users",key="#pageable.pageSize")
 	public Page<User> findUserByPage(Pageable pageable) {
 		return this.userRepository.findAll(pageable);
 	}
 
 	@Override
+	//@CacheEvict(value="users",allEntries=true) 清除缓存中以users缓存策略缓存的对象
+	@CacheEvict(value="users",allEntries=true)
 	public void saveUser(User user) {
 		this.userRepository.save(user);
 	}
--- springboot_cache/springboot_ehcache/src/test/java/com/jason/test/UserServiceTest.java
+++ springboot_cache/springboot_ehcache/src/test/java/com/jason/test/UserServiceTest.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.jason.App;
+import com.jason.pojo.User;
 import com.jason.service.UserService;
 
 /**
@@ -15,18 +18,47 @@ import com.jason.service.UserService;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes=App.class)
+@SpringBootTest(classes = App.class)
 public class UserServiceTest {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@Test
-	public void testFindUserById(){
-		//第一次查询
+	public void testFindUserById() {
+		// 第一次查询
 		System.out.println(this.userService.findUserById(1));
-		
-		//第二次查询
+
+		// 第二次查询
 		System.out.println(this.userService.findUserById(1));
+	}
+
+	@Test
+	public void testFindUserByPage() {
+		Pageable pageable = new PageRequest(0, 2);
+		// 第一次查询
+		System.out.println(this.userService.findUserByPage(pageable).getTotalElements());
+
+		// 第二次查询
+		System.out.println(this.userService.findUserByPage(pageable).getTotalElements());
+
+		// 第三次查询
+		pageable = new PageRequest(1, 2);
+		System.out.println(this.userService.findUserByPage(pageable).getTotalElements());
+	}
+
+	@Test
+	public void testFindAll() {
+		// 第一次查询
+		System.out.println(this.userService.findUserAll().size());
+
+		User users = new User();
+		users.setAddress("南京");
+		users.setAge(43);
+		users.setName("朱七");
+		this.userService.saveUser(users);
+
+		// 第二次查询
+		System.out.println(this.userService.findUserAll().size());
 	}
 }
